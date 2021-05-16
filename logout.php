7f5d91264e7d9582e686f1fe8eb5aaed36078718
chinmayj02 <chinmayujoshi@gmail.com>
2021-05-16 15:54:15 +0530

    logout

--- logout.php
+++ logout.php
@@ -1,7 +1,9 @@
 <?php
     session_start();
-    if(!isset($_SESSION["email"])){
-		header("location: index.php");}
+    if(!isset($_SESSION["email"]))
+    {
+		header("location: index.php");
+    }
     session_destroy();
     header("location: index.php");
 ?>
