8f83b3ff2f9a6228d0e1f12f788935dc20fcf7ce
chinmayj02 <chinmayujoshi@gmail.com>
2021-05-19 21:43:28 +0530

    final submission

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "serverpass123", "e-commerce") or die(mysqli_error($conn));
+    $conn = mysqli_connect("localhost", "root", "", "LifestyleStore") or die(mysqli_error($conn));
     if(!isset($_SESSION['email'])){
     session_start();
     }
